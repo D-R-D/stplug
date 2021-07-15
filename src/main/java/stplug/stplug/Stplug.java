ce33fa86b2ba24d7e93f8904ece590d5c25f230a
D-R-D <taiki.20010322@gmail.com>
2021-07-15 18:20:07 +0900

    mcp1.17.1に対応、ポート7001,7011で管理用の通信を追加

--- src/main/java/stplug/stplug/Stplug.java
+++ src/main/java/stplug/stplug/Stplug.java
@@ -1,24 +1,32 @@
 package stplug.stplug;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public final class Stplug extends JavaPlugin implements Listener
 {
-
+    boolean bool = false;
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(this,this);
+        eventsender("mcp_h:started");
+        sinsreceiver();
     }
 
     @Override
@@ -26,10 +34,19 @@ public final class Stplug extends JavaPlugin implements Listener
         // Plugin shutdown logic
     }
 
+    @EventHandler
+    public void PlayerJoinEvent(PlayerJoinEvent e)
+    {
+        if(bool) {
+            Player p = e.getPlayer();
+            p.sendMessage(ChatColor.RED + "sys_Message : サーバーはアップデート中です。5分後に再度ログインして下さい。");
+        }
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command,String label, String[] args) {
 
-        if (command.getName().equalsIgnoreCase("container")) {
+        if (command.getName().equalsIgnoreCase("container") && !bool) {
             if (args.length == 0) {
                 sender.sendMessage("サブコマンドを入力してください。");
                 return false;
@@ -41,6 +58,7 @@ public final class Stplug extends JavaPlugin implements Listener
                     } else {
                         String cmd = "container:start:" + args[1];
                         sinssender(cmd);
+                        eventsender(args[1] +":starting");
                     }
                 } else if (args[0].equalsIgnoreCase("stop")) {
                     if (args[1] == null) {
@@ -49,13 +67,13 @@ public final class Stplug extends JavaPlugin implements Listener
                     } else {
                         String cmd = "container:stop:" + args[1];
                         sinssender(cmd);
+                        eventsender(args[1] +":stopping");
                     }
                 } else {
                     sender.sendMessage("サブコマンドが違います。");
                     return false;
                 }
             }
-
         }
         return true;
     }
@@ -77,5 +95,59 @@ public final class Stplug extends JavaPlugin implements Listener
         sock.close();
 
         getLogger().info("udp sended");
+    }
+
+    public void eventsender(String str)
+    {
+        //strをbyte配列に変換
+        byte[] data;
+        data = str.getBytes(StandardCharsets.UTF_8);
+        //接続用のソケットを作成
+        DatagramSocket sock = null;
+        try { sock = new DatagramSocket(); }
+        catch (SocketException e) { e.printStackTrace();}
+        //パケットを作成し、udpで送信する。
+        DatagramPacket packet = new DatagramPacket(data,data.length,new InetSocketAddress("127.0.0.1",7011));
+        try { sock.send(packet); }
+        catch (IOException e) { e.printStackTrace(); }
+        //最後にお片付けして終了
+        sock.close();
+
+        getLogger().info("event sended");
+    }
+
+    public  void sinsreceiver()
+    {
+        //データ受信
+        byte[] data = new byte[1024];
+        DatagramSocket sock = null;
+        try {
+            sock = new DatagramSocket(7001);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+
+        while (true) {
+            try {
+                sock.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String str = null;
+            try {
+                str = new String(Arrays.copyOf(packet.getData(), packet.getLength()), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            getLogger().info(str);
+
+            //データごとに処理
+            if(str.equals("reboot"))
+            {
+                bool = true;
+            }
+        }
     }
 }
